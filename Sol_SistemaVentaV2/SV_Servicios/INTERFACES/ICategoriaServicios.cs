f1a509bcfd7d837de660bb20ea51489057ea6714
enigmaticspac3 <jgonzreal62@gmail.com>
2024-12-03 23:59:41 -0500

    modified CapaServicios with its necessary functions in each Entity (INTERFACES - IMPLEMENTACION)

--- Sol_SistemaVentaV2/SV_Servicios/INTERFACES/ICategoriaServicios.cs
+++ Sol_SistemaVentaV2/SV_Servicios/INTERFACES/ICategoriaServicios.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SV_Repositorio.ENTIDADES;
 
 namespace SV_Servicios.INTERFACES
 {
-    internal interface ICategoriaServicios
+    public interface ICategoriaServicios
     {
+        Task<List<Categoria>> listaCategoria(string buscar = "");//Nombre del objeto
+        Task<string> CrearCategoria(Categoria objeto);
+        Task<string> EditarCategoria(Categoria objeto);
     }
 }
